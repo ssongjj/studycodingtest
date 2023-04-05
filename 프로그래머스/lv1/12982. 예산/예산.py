281f9d775579d1c4bc5134948e7f01ebf97af06e
ssongjj <39427152+ssongjj@users.noreply.github.com>
2023-04-05 20:53:53 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.py
+++ 프로그래머스/lv1/12982. 예산/예산.py
@@ -1,14 +1,22 @@
 #전체 예산이 정해져 있음 -> 최대한 많은 부서의 물품을 구매
 #물품을 구매할 때는 일부만 줄 수 없고 신청한 금액 모두를 줘야 함
-#sort 후 작은 수부터 budget에서 빼 주고 만약 다음 부서의 예산이 남은 budget보다 클 경우 종료
 def solution(d, budget):
     answer = 0
     d.sort()
     
+    #1 sort 후 작은 수부터 budget에서 빼 주고 만약 다음 부서의 예산이 남은 budget보다 클 경우 종료
+    '''
     for n in d:
         if budget - n < 0:
             break 
         budget -= n
-        answer += 1
-        
+        answer += 1 
+    
     return answer
+    '''
+    
+    #2 sort 후 d의 합이 budget보다 크면 제일 부서 예산이 큰 곳부터 제거 후 남는 부서를 계산
+    while budget < sum(d):
+        d.pop()
+    
+    return len(d)
