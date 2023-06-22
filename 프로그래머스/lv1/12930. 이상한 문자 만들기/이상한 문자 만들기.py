f93b8b91756e75227877fc253140bfad0a52072b
JIHYE SONG <39427152+ssongjj@users.noreply.github.com>
2023-06-22 23:19:32 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,3 +1,16 @@
 def solution(s):
     answer = ''
+    l = s.split(" ")
+    a_lst = []
+    for ch in l:
+        st = ""
+        for i, c in enumerate(ch):
+            if i%2 != 0:
+                st += c.lower()
+            else:
+                st += c.upper()
+        a_lst.append(st)
+    answer = ' '.join(a_lst)
+    
+    
     return answer
