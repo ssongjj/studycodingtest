313b064f277b1c1d2edf9c5fdc371016f89f00e0
JIHYE SONG <39427152+ssongjj@users.noreply.github.com>
2023-06-27 22:27:24 +0900

    Update H－Index.py

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,10 +1,9 @@
 def solution(citations):
     # H-index n편 중 h번 이상 인용 논문 h편 이상 / 나머지 논문이 h번 이하 인용 => h의 최댓값
-    # 발표한 논문의 수와 인용된 논문의 수가 같을 때
+    # 발표한 논문의 수와 인용된 논문의 수가 같을 때 H-index 개념 참고
     citations.sort()
     for i, citation in enumerate(citations):
         if citation >= len(citations)-i:
             return len(citations)-i
-            
-        
+
     return 0
