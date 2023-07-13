c3acb1c2084697986d1e59e074d2c1cd44cf7e45
JIHYE SONG <39427152+ssongjj@users.noreply.github.com>
2023-07-13 22:50:08 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = 0
+    alph_dic = { "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+    
+    for a_num, num in alph_dic.items():
+        s = s.replace(a_num, str(num))
+        
+    answer = int(s) 
+    return answer
