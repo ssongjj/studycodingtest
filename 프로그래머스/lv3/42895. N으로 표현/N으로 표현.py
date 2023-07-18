6403469d6d25d29517c3037c3f442042cddd3ab9
JIHYE SONG <39427152+ssongjj@users.noreply.github.com>
2023-07-18 23:28:15 +0900

    [level 3] Title: N으로 표현, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -1,27 +1,4 @@
-answer = -1
 
-def DFS(n, pos, num, number, s):
-    global answer
-    if pos > 8:
-        return
-    if num == number:
-        if pos < answer or answer == -1:
-            #print(s)            
-            answer=pos
-        return
-
-    nn=0
-    for i in range(8):
-        nn=nn*10+n
-        DFS(n, pos+1+i, num+nn, number, s+'+')
-        DFS(n, pos+1+i, num-nn, number, s+'-')
-        DFS(n, pos+1+i, num*nn, number, s+'*')
-        DFS(n, pos+1+i, num/nn, number, s+'/')
-
-def solution(N, number):    
-    DFS(N, 0, 0, number, '')    
-    return answer
-'''
 def solution(N, number):
     answer = 0
     s = [set() for x in range(8)]
@@ -48,4 +25,3 @@ def solution(N, number):
         answer = -1
             
     return answer
-'''
