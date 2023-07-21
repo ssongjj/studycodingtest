4ffbd125bad9aa73817dd6f2a0e00193a7735156
JIHYE SONG <39427152+ssongjj@users.noreply.github.com>
2023-07-21 23:22:41 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,34 @@
+def solution(n, costs):
+    #[a, b, c] a와 b는 연결되어 있고 c는 비용
+    # 최소 cost를 찾아야 함으로 크루스칼 알고리즘
+    
+    answer = 0
+    parent = [0] * (n+1)
+    for i in range(1, n+1):
+        parent[i] = i 
+    #cost가 낮은 순으로 sort
+    costs.sort(key = lambda x: x[2])
+    
+    #정렬된 간선 정보를 하나씩 확인하면서 사이클을 발생시키는지 확인
+    #사이클이 발생하지 않으면 최소 신장 트리에 포함
+    def find_parent(x):
+        if parent[x] != x:
+            parent[x] = find_parent(parent[x])
+        else:
+            return x
+        return parent[x]
+    
+    def union_parent(a, b):
+        a = find_parent(a)
+        b = find_parent(b)
+        
+        if a < b:
+            parent[b] = a
+        else:
+            parent[a] = b
+            
+    for n1, n2, cost in costs:
+        if find_parent(n1) != find_parent(n2):
+            union_parent(n1, n2)
+            answer += cost
+    return answer
