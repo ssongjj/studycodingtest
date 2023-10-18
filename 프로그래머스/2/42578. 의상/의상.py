3802d0c9b11cac87b567e84dd9480f1bb26a6a0e
JIHYE SONG <39427152+ssongjj@users.noreply.github.com>
2023-10-18 18:19:16 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,33 @@
+def solution(clothes):
+    '''
+    answer = 1
+    dict = {key:[] for _,key in clothes}
+    
+    for v, k in clothes:
+        dict[k].append(v)
+    
+    if len(dict.keys()) == 1:
+        answer = len(clothes)
+        return answer
+    
+    for values in dict.values():
+        answer *= (len(values) + 1)
+    # +1을 해 주는 이유: 입지 않는 경우도 포함    
+    return answer - 1 
+    # 둘 다 입지 않는 경우의 수가 존재하기 때문에 총 경우의 수에서 -1 처리해 줌
+    '''
+    
+    answer = 1
+    dict = {key: [] for _, key in clothes}
+    
+    for v, k in clothes:
+        dict[k].append(v)
+        
+    if len(dict.keys()) == 1:
+        answer = len(clothes)
+        return answer
+    
+    for values in dict.values():
+        answer *= (len(values) + 1)
+    
+    return answer - 1
