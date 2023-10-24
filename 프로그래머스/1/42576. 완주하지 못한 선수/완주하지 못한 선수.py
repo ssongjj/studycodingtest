a1f91877315236eb871f87e57919009851892125
JIHYE SONG <39427152+ssongjj@users.noreply.github.com>
2023-10-24 21:38:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.56 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -4,6 +4,7 @@ def solution(participant, completion):
     p_counter = collections.Counter(participant)
     c_counter = collections.Counter(completion)
     
+    # 카운터끼리는 빼기가 가능하다
     nc_counter = p_counter - c_counter
     answer = ''.join(nc_counter.keys())
     return answer
