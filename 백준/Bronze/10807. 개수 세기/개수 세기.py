f77e0b3ca86746d4a719335ac689321489e22527
JIHYE SONG <39427152+ssongjj@users.noreply.github.com>
2024-03-10 00:27:41 +0900

    [Bronze V] Title: 개수 세기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,9 +1,5 @@
-from collections import Counter
-
 n = int(input())
 n_list = list(map(int, input().split()))
 v = int(input())
 
-counter = Counter(n_list)
-
-print(counter[v])
+print(n_list.count(v))
