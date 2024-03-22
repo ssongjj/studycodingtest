0c73ae44f1299b68340b8d2c909dc15df49505ec
JIHYE SONG <39427152+ssongjj@users.noreply.github.com>
2024-03-22 20:11:24 +0900

    [Silver V] Title: 방 번호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,14 +1,12 @@
-from collections import Counter
-
 n = input()
-num_list = [int(num) for num in n]
-
-num_counter = Counter(num_list)
-
+num_counter = [0] * 10
 # 6과 9는 서로 대체 가능하다
 # 하지만 홀수인 경우에도 하나는 추가가 되어야 하기 때문에 +1
-num_counter[6] = (num_counter[6] + num_counter[9] + 1)//2
-num_counter[9] -= num_counter[9]
-answer = num_counter.most_common(1)[0][1]
+for num in n:
+    if num != '9':
+        num_counter[int(num)] += 1
+    else:
+        num_counter[6] += 1
+num_counter[6] = (num_counter[6] + 1) // 2
 
-print(answer)
+print(max(num_counter))
