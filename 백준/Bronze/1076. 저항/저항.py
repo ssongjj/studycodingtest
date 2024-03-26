be117116672437a273f5d1ad76f3163efd930a69
JIHYE SONG <39427152+ssongjj@users.noreply.github.com>
2024-03-27 02:16:55 +0900

    [Bronze II] Title: 저항, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,19 @@
+colors = {
+    "black": (0, 1),
+    "brown": (1, 10),
+    "red": (2, 100),
+    "orange": (3, 1000),
+    "yellow": (4, 10000),
+    "green": (5, 100000),
+    "blue": (6, 1000000),
+    "violet": (7, 10000000),
+    "grey": (8, 100000000),
+    "white": (9, 1000000000)
+}
+
+color1 = input()
+color2 = input()
+color3 = input()
+
+res = (colors[color1][0] * 10 + colors[color2][0]) * colors[color3][1]
+print(res)
