a562fcce082b5580aecaf3fcb9f141171e7c27c1
JIHYE SONG <39427152+ssongjj@users.noreply.github.com>
2024-05-22 21:23:54 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 172.28 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -7,27 +7,27 @@ def solution(rows, columns, queries):
 
     for q in queries:
         q = [x - 1 for x in q]
-        y1, x1, y2, x2 = q
-        tmp = matrix[y1][x1]
+        x1, y1, x2, y2 = q
+        tmp = matrix[x1][y1]
         small = tmp
 
-        for i in range(y1 + 1, y2 + 1):
-            matrix[i - 1][x1] = matrix[i][x1]
-            small = min(small, matrix[i][x1])
-
         for i in range(x1 + 1, x2 + 1):
-            matrix[y2][i - 1] = matrix[y2][i]
-            small = min(small, matrix[y2][i])
+            matrix[i - 1][y1] = matrix[i][y1]
+            small = min(small, matrix[i][y1])
 
-        for i in range(y2 - 1, y1 - 1, -1):
-            matrix[i + 1][x2] = matrix[i][x2]
-            small = min(small, matrix[i][x2])
+        for i in range(y1 + 1, y2 + 1):
+            matrix[x2][i - 1] = matrix[x2][i]
+            small = min(small, matrix[x2][i])
 
         for i in range(x2 - 1, x1 - 1, -1):
-            matrix[y1][i + 1] = matrix[y1][i]
-            small = min(small, matrix[y1][i])
+            matrix[i + 1][y2] = matrix[i][y2]
+            small = min(small, matrix[i][y2])
+
+        for i in range(y2 - 1, y1 - 1, -1):
+            matrix[x1][i + 1] = matrix[x1][i]
+            small = min(small, matrix[x1][i])
         
-        matrix[y1][x1 + 1] = tmp
+        matrix[x1][y1 + 1] = tmp
 
         answer.append(small)
 
